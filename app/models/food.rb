78843eeb67b2ef90fec3e1f53f9504b66f7f6fd9
takanosatomi <ricebread778899@gmail.com>
2024-10-02 21:56:14 +0900

    check 2 (food.rb)

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,11 +1,7 @@
 class Food < ApplicationRecord
+  #
   has_many :order_foods
 
   belongs_to :shop
 
-  has_many :japanesefood, as: :country_foodable
-
-  has_many :italianfood, as: :country_foodable
-
-  has_many :chinesefood, as: :country_foodable
 end
