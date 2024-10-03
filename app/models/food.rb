47f95ab24ec3765536842428c6d255959014f381
takanosatomi <ricebread778899@gmail.com>
2024-10-03 11:43:13 +0900

    多対多

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,6 @@ class Food < ApplicationRecord
 
   belongs_to :shop
 
+  has_and_belongs_to_many :orders
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
 
   belongs_to :customer
   belongs_to :address
+
+  has_and_belongs_to_many :foods
 end
